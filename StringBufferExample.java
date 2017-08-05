eb1e736f8ef02d4e8841bb97841cc978ba2badde
krishnapattar <krishnapattar140@gmail.com>
2017-08-05 18:02:20 +0530

    Add files via upload

---
+++ StringBufferExample.java
@@ -0,0 +1,21 @@
+
+	/**
+	* Write a program in java to show how the capacity is changing in StringBuffer or in StringBuilder if we
+append the string.
+	*/
+	class StringBufferExample{  //proper naming conventions is used 
+		
+public static void main(String args[]){ // Main methods starts
+	
+StringBuffer sb=new StringBuffer();  
+System.out.println(sb.capacity());//default 16  
+sb.append("Hello");  
+System.out.println(sb.capacity());//now 16  
+sb.append("java is my favourite language");  
+System.out.println(sb.capacity());//now (16*2)+2=34 i.e (oldcapacity*2)+2  
+sb.ensureCapacity(10);//now no change  
+System.out.println(sb.capacity());//now 34  
+sb.ensureCapacity(50);//now (34*2)+2  
+System.out.println(sb.capacity());//now 70  
+}  
+}  // class closed
